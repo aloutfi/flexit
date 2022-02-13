6365624e9fc7492999254f9e1668946aa6d7d3b9
Andrew Loutfi <loofinator@gmail.com>
2022-02-13 14:46:29 -0600

    feature: added dto to query

--- flexit/queries.py
+++ flexit/queries.py
@@ -1,15 +1,15 @@
 from flexit.config import engine
-from flexit import models
+from flexit import models, dto
 
 from sqlalchemy.orm import Session
 
 
-def shows_of_actor(actor: str) -> list[models.Show | None]:
+def shows_of_actor(actor: str) -> list[dto.Show | None]:
     """Return the shows that the given actor has been in."""
     with Session(engine) as session:
         person = session.query(models.Person).filter_by(person=actor).one()
         return [
-            result.show
+            dto.Show.from_orm(result.show)
             for result in session.query(models.ShowCastIntersection)
             .filter_by(person_id=person.person_id)
             .all()
