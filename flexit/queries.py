ae213ec0dccc4cf45f5c99e1be07b8ecab8063c4
Andrew Loutfi <loofinator@gmail.com>
2022-02-14 18:57:52 -0600

    feat: added method declarations for remaining queries

--- flexit/queries.py
+++ flexit/queries.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flexit.config import engine
 from flexit import models, dto
 
@@ -11,6 +13,34 @@ def shows_of_actor(actor: str) -> list[dto.Show | None]:
         return [
             dto.Show.from_orm(result.show)
             for result in session.query(models.ShowCastIntersection)
-            .filter_by(person_id=person.person_id)
+            .filter_by(person=person)
             .all()
         ]
+
+
+def shows_in_category(category: str) -> list[dto.Show | None]:
+    """Return the shows that fall within the given category."""
+    with Session(engine) as session:
+        category = session.query(models.Category).filter_by(category).one()
+        return [
+            dto.Show.from_orm(result.show)
+            for result in session.query(models.ShowCategoryIntersection)
+            .filter_by(category=category)
+            .all()
+        ]
+
+
+def person_is_director_and_actor(person: str) -> bool:
+    """Return whether the person is a director and actor."""
+
+
+def person_directed_and_acted_in_same_show(person: str) -> list[dto.Show | None]:
+    """Return shows that the person both directed and acted in."""
+
+
+def shows_added_on_date(date: datetime.date) -> list[dto.Show | None]:
+    """Return shows that were added on a given date."""
+
+
+def high_level_stats() -> dict:
+    """Return number of tv shows, number of movies, total number of categories, shows released by year."""
--- tests/test_flexit_queries.py
+++ tests/test_flexit_queries.py
@@ -1,10 +1,12 @@
+from datetime import datetime
+
 import pytest
 from flexit import queries
 
 
 class TestQueries:
     @pytest.mark.parametrize("actor", ["Kevin Bacon", "Trey Parker", "Kyle MacLachlan"])
-    def test_shows_of_actor(self, actor):
+    def test_shows_of_actor(self, actor: str):
         """Ensure expected results for shows_of_actor query."""
         shows = queries.shows_of_actor(actor)
 
@@ -18,3 +20,12 @@ class TestQueries:
             case _:
                 expected_shows = 0
         assert len(shows) == expected_shows
+
+    def test_person_is_director_and_actor(self, person: str):
+        """Ensure expected results for person_is_director_and_actor."""
+
+    def test_person_directed_and_acted_in_same_show(self, person: str):
+        """Ensure expected results for person_directed_and_acted_in_same_show query."""
+
+    def test_shows_added_on_date(self, date: datetime.date):
+        """Ensure expected results for test_shows_added_on_date query."""
