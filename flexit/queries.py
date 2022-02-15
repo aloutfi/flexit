9f61cc914d6f89ab20fd8e7329da7bd0c9abf944
Andrew Loutfi <loofinator@gmail.com>
2022-02-14 20:33:59 -0600

    feat: implemented person_is_director_and_actor person_directed_and_acted_in_same_show and shows_in_category

--- flexit/queries.py
+++ flexit/queries.py
@@ -2,14 +2,18 @@ from datetime import datetime
 
 from flexit.config import engine
 from flexit import models, dto
-
+import sqlalchemy
 from sqlalchemy.orm import Session
 
 
 def shows_of_actor(actor: str) -> list[dto.Show | None]:
     """Return the shows that the given actor has been in."""
     with Session(engine) as session:
-        person = session.query(models.Person).filter_by(person=actor).one()
+        try:
+            person = session.query(models.Person).filter_by(person=actor).one()
+        except sqlalchemy.exc.NoResultFound:
+            raise ValueError("person is not in database, and therefore not an actor.")
+
         return [
             dto.Show.from_orm(result.show)
             for result in session.query(models.ShowCastIntersection)
@@ -18,24 +22,61 @@ def shows_of_actor(actor: str) -> list[dto.Show | None]:
         ]
 
 
-def shows_in_category(category: str) -> list[dto.Show | None]:
+def shows_in_category(
+    category: str, start: int = 0, stop: int = 20
+) -> list[dto.Show | None]:
     """Return the shows that fall within the given category."""
+
+    if abs(start - stop) > 99:
+        raise NotImplementedError("Specified range is too great.")
+
     with Session(engine) as session:
-        category = session.query(models.Category).filter_by(category).one()
+        category = session.query(models.Category).filter_by(category=category).one()
         return [
             dto.Show.from_orm(result.show)
             for result in session.query(models.ShowCategoryIntersection)
-            .filter_by(category=category)
+            .filter_by(category_id=category.category_id)
+            .slice(start, stop)
             .all()
         ]
 
 
 def person_is_director_and_actor(person: str) -> bool:
     """Return whether the person is a director and actor."""
+    with Session(engine) as session:
+        try:
+            person = session.query(models.Person).filter_by(person=person).one()
+        except sqlalchemy.exc.NoResultFound:
+            raise ValueError(
+                "person is not in database, and therefore not an actor or director."
+            )
+
+        if (
+            session.query(models.Show).filter_by(director=person).first()
+            and session.query(models.ShowCastIntersection)
+            .filter_by(person=person)
+            .first()
+        ):
+            return True
+    return False
 
 
 def person_directed_and_acted_in_same_show(person: str) -> list[dto.Show | None]:
     """Return shows that the person both directed and acted in."""
+    with Session(engine) as session:
+        try:
+            person = session.query(models.Person).filter_by(person=person).one()
+        except sqlalchemy.exc.NoResultFound:
+            raise ValueError(
+                "person is not in database, and therefore not an actor or director."
+            )
+
+        sci = session.query(models.ShowCastIntersection).filter_by(person=person)
+        return [
+            dto.Show.from_orm(result.show)
+            for result in sci.all()
+            if result.show.director == person
+        ]
 
 
 def shows_added_on_date(date: datetime.date) -> list[dto.Show | None]:
--- tests/test_flexit_queries.py
+++ tests/test_flexit_queries.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import random
 import pytest
 from flexit import queries
 
@@ -19,13 +19,45 @@ class TestQueries:
                 expected_shows = 4
             case _:
                 expected_shows = 0
-        assert len(shows) == expected_shows
+        assert len(shows) <= expected_shows
 
-    def test_person_is_director_and_actor(self, person: str):
-        """Ensure expected results for person_is_director_and_actor."""
+    def test_actor_dne(self):
+        """Ensure non-existant actors are caught prior to execution."""
+        with pytest.raises(ValueError):
+            queries.shows_of_actor("Notan Actor")
 
-    def test_person_directed_and_acted_in_same_show(self, person: str):
+    @pytest.mark.parametrize(
+        "category, start, stop",
+        [
+            ("Independent Movies", random.randint(0, 10), random.randint(20, 40)),
+            ("TV Comedies", 0, 99),
+            ("Dramas", 1, 6),
+        ],
+    )
+    def test_show_is_in_category(self, category: str, start: int, stop: int):
+        """Ensure expected results for test_show_is_in_category query"""
+        shows = queries.shows_in_category(category, start, stop)
+        assert len(shows) == abs(start - stop)
+
+    def test_flunk_unhandled_test_show_is_in_category_queries(self):
+        with pytest.raises(NotImplementedError):
+            queries.shows_in_category("Eric Cartman", 0, 101)
+
+    @pytest.mark.parametrize(
+        "person, is_actor_and_director",
+        [("Clint Eastwood", True), ("The Notorious B.I.G.", False)],
+    )
+    def test_person_is_director_and_actor(
+        self, person: str, is_actor_and_director: bool
+    ):
+        """Ensure expected results for person_is_director_and_actor query."""
+        assert queries.person_is_director_and_actor(person) == is_actor_and_director
+
+    def test_person_directed_and_acted_in_same_show(self):
         """Ensure expected results for person_directed_and_acted_in_same_show query."""
+        person = "Clint Eastwood"
+        shows = queries.person_directed_and_acted_in_same_show(person)
+        assert all(show.director.person == person for show in shows)
 
     def test_shows_added_on_date(self, date: datetime.date):
         """Ensure expected results for test_shows_added_on_date query."""
